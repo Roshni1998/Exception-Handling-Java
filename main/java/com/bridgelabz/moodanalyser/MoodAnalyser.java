070291d836f7402d18c9076fbb7cdbbaa0747a1b
Roshni1998 <roshnimali847@gmail.com>
2021-06-21 23:29:21 +0530

    Mood Analyser UC-1

--- main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
-    public static void main(String [] args){
-        System.out.println("Welcome to Mood Analyser");
+
+    public String analyseMood(String message) {
+        if(message.contains("SAD"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
 
